65da88d20b6d4ee3b2307fce006ac5cc75e8141d
Absolf <victorlaureanocg@gmail.com>
2019-11-11 16:13:33 +0000

    Copy time from master using website

--- Time.h
+++ Time.h
@@ -4,29 +4,50 @@
 
 #ifndef AEDA_PROJETO_TIME_H
 #define AEDA_PROJETO_TIME_H
-using namespace std;
-#include "string"
+
+#include <sstream>
+#include <string>
 #include "CartaoAmigo.h"
 
+using namespace std;
+
 class Time {
+    CartaoAmigo novo;
     int hora;
     int minutos;
     int segundos;
 public:
-
     Time();
     int getHora() const;
-
     void setHora(int hora);
-
     int getMinutos() const;
-
     void setMinutos(int minutos);
-
     int getSegundos() const;
     void setSegundos(int segundos);
 
-};
 
+    //-------------------------------
+    bool valid();
+
+    void setTime(int hora, int minutos, int segundos);
+    void setTimeString(string linha);//set Time throw a string
+    string getTimeString(); //???
+    //bool verifyTime(string time); verifica a func de cima.
+
+    //other methods
+    //static string returnTime(Time time); //Displays the date in a string
+    friend ostream &operator<<(ostream &out, const Time &time); //output operator
+    bool operator ==(const Time&t); // comparison operators
+    bool operator != (const Time&t);
+    bool operator >(const Time&t);
+    bool operator <(const Time&t);
+
+    //------------------------------//
+    Time operator-(const Time& t);
+    //Time operator+(const Time& t2);
+
+
+
+};
 
 #endif //AEDA_PROJETO_TIME_H
